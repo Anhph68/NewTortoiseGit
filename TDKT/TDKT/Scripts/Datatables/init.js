b5eb152725ba54cf1e70c3cf1be66d4cf9736078
Anh <Anhph68@gmail.com>
2014-06-18 07:40:52 +0700

    Manage User 2

---
+++ TDKT/TDKT/Scripts/Datatables/init.js
@@ -0,0 +1,20 @@
+﻿var oTable;
+$.extend($.fn.DataTable.defaults, {
+    "paginationType": "full_numbers",
+    "language": {
+        "search": "Tìm kiếm: ",
+        "decimal": ",",
+        "thousands": ".",
+        "lengthMenu": "Hiển thị _MENU_ kết quả/trang",
+        "zeroRecords": "Không tìm thấy kết quả nào phù hợp",
+        "info": "Đang xem _START_ đến _END_ trong tổng số _TOTAL_ kết quả",
+        "infoEmpty": "Không tìm thấy kết quả nào phù hợp",
+        "infoFiltered": "(được lọc từ _MAX_ cuộc)",
+        "paginate": {
+            "first": '<span class="glyphicon glyphicon-fast-backward"></span>',
+            "previous": '<span class="glyphicon glyphicon-backward"></span>',
+            "next": '<span class="glyphicon glyphicon-forward"></span>',
+            "last": '<span class="glyphicon glyphicon-fast-forward"></span>'
+        }
+    }
+});
